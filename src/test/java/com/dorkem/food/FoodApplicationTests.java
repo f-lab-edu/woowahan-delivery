d705267ab449438d319d8eb2be658a9c341e8aec
dorkem <20233144@gs.cwnu.ac.kr>
2026-01-21 22:46:05 +0900

    :sparkles: init Spring

---
+++ src/test/java/com/dorkem/food/FoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dorkem.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
