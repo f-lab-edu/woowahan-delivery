a771bae055c82389f79d1b5d8eb21e2f774a3a48
dorkem <20233144@gs.cwnu.ac.kr>
2026-04-10 19:42:47 +0900

    :feature: 메뉴 및 가게의 오픈 등 상태관리

--- src/main/java/com/dorkem/food/menu/entity/Menu.java
+++ src/main/java/com/dorkem/food/menu/entity/Menu.java
@@ -49,6 +49,14 @@ public class Menu {
 	@Column(name = "price", nullable = false)
 	private int price;
 
+	@Getter
+	@Column(name = "is_sold_out", nullable = false)
+	private boolean isSoldOut = false;
+
+	public void toggleSoldOut() {
+		this.isSoldOut = !this.isSoldOut;
+	}
+
 	@CreatedDate
 	@Column(name = "created_at", nullable = false)
 	private LocalDateTime createdAt;
