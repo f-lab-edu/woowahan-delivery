d1f2f7c18e26754e46c0444054b8c4060bed7a0c
dorkem <20233144@gs.cwnu.ac.kr>
2026-04-10 19:52:54 +0900

    :feature: 오너 찾는 레포지토리 작성

--- src/main/java/com/dorkem/food/user/repository/OwnerRepository.java
+++ src/main/java/com/dorkem/food/user/repository/OwnerRepository.java
@@ -1,10 +1,15 @@
 package com.dorkem.food.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.dorkem.food.user.entity.Owner;
+import com.dorkem.food.user.entity.User;
 
 @Repository
 public interface OwnerRepository extends JpaRepository<Owner, Long> {
+
+	Optional<Owner> findByUser(User user);
 }
