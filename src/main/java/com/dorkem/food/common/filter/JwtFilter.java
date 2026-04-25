3a69f377d9ea76e918aa6be98cd1161a6a4fbc05
dorkem <20233144@gs.cwnu.ac.kr>
2026-04-26 00:53:41 +0900

    :recycle: 부하테스트시 로그인 인증 건너뛰기 위해

--- src/main/java/com/dorkem/food/common/filter/JwtFilter.java
+++ src/main/java/com/dorkem/food/common/filter/JwtFilter.java
@@ -33,7 +33,8 @@ public class JwtFilter extends OncePerRequestFilter {
 		"/api/v1/owner/auth/login",
 		"/v3/api-docs",
 		"/swagger-ui",
-		"/h2-console"
+		"/h2-console",
+		"/api/v1/stores/test"
 	);
 
 	@Override
